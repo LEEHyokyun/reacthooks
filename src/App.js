0b6432a4234bab47ed71509e797fd3039f85ff9e
hyokyun <gyrbs22@gmail.com>
2021-08-23 10:29:15 +0900

    before using hooks, as class programming

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {count : 0}
+
+  plus= () => {
+    this.setState({count : this.state.count + 1})
+  }
+
+
+  render() {
+    const {count} = this.state
+    return(
+    <>
+    <div className="App">{count}</div>
+    <button onClick={this.plus}>PLUS</button>
+    </>
+    )
+  };
 }
 
 export default App;
