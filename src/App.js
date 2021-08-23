5d81438ae2d79833644df240db62a2b6ccb24586
hyokyun <gyrbs22@gmail.com>
2021-08-23 11:30:57 +0900

    after using hooks, state for email input string

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import React, {Component, useState} from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [email, setEmail ] = useState('')
+  const updateEmail = e => {
+    const { target : {value} } = e
+    setEmail(value)
+  }
   return(
     <>
     {count}
     <button onClick={()=>setCount(count+1)}>PLUS</button>
+    <input placeholder="EMAIL" value={email} onChange={updateEmail}/>
     </>
   )
 }
