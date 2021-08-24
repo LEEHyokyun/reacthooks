2ced255b47eb0b73a0cd8ad81a923c7e42ccb44d
hyokyun <gyrbs22@gmail.com>
2021-08-24 13:46:53 +0900

    useClick, by using useRef/useEffect - componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+
+const useClick = (afterClicked) => {
+
+  const element = useRef();
+  useEffect( ()=> { //after rendering, run
+    if(element.current){ 
+      console.log(element.current)
+      //it is not log, just adding event to DOM with specific function(=onClick)
+      element.current.addEventListener("click", afterClicked)
+    }
+  })
+  console.log(element.current) //initially run, this would be "undefined"
+  return element
+}
 
 const App = () => {
-
- 
+  const clicked = () => console.log("Clicked!")
+  const title = useClick(clicked)
+  
   return(
     <div className="App">
+      <h2 ref={title}>Hello</h2>
     </div>
   )
 }
